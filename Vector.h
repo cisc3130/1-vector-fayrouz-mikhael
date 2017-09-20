0bdb084b5687b6c0b900239e5973c8c47275e3f6
fayrouz-mikhael <31423114+fayrouz-mikhael@users.noreply.github.com>
2017-09-19 22:13:32 -0500

    update Vector.h by completing the missing function
    @rivlev

--- Vector.h
+++ Vector.h
@@ -1,73 +1,115 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+ using namespace std;
 
 template <class T>
 class Vector {
   T* arr;
   int sz, capacity;
-
-  // convenience function called by push_back and insert.
-  // allocate a new array with double the capacity,
-  // copy arr over the the new array,
-  // deallocate arr,
-  // point arr to the new array,
-  // update capacity
+ 
   void grow() {
+  	capacity = capacity *2; // allocate a new array with double the capacity,
+  	T*temp = new T[capacity];
+  	for(int i=0 ; i< sz ; i++){
+  		temp[i]=arr[i]; // copy arr over the the new array,
+  		
+	  }
+	  delete []arr; // deallocate arr,
+	  arr =temp;  // point arr to the new array,
   }
 
   public:
-  // allocate a new array with size 0 (nothing is in it yet)
-  // and capacity 10
-  // set sz, capacity, arr
-  Vector() {
+  Vector() { //allocate a new array with size 0 (nothing is in it yet) 
+  	sz=0;
+  	arr= new T[10];
+  	capacity = 10; //and capacity 10
   }
 
-  // deallocate arr
-  ~Vector() {
+  
+  ~Vector() {  // deallocate arr
+  	if(arr!= NULL)
+  	delete [] arr;
   }
 
-  // add elt to the end of arr (remember to update sz!)
-  void push_back(const T& elt) {
+  
+  void push_back(const T& elt) {  // add elt to the end of arr (remember to update sz!)
+  if(sz== capacity){ // if size = the capacity it should grow the array
+  	grow ();
+  }
+  	arr[sz]=elt; // else put elt at the back of the array 
+  	sz++; // incerment the size
   }
 
-  // remove the last element in arr (remember to update sz!)
-  void pop_back() {
-  }
+  
+  void pop_back() { // remove the last element in arr (remember to update sz!)
+  	if(!empty()) // check if the array is empty
+  		sz--;  // decrement the size
+}
+  
 
-  // add elt to arr at position pos
-  // throw an exception if pos is not legal
-  // remember to update sz
-  void insert(const T& elt, int pos) {
-  }
-
-  // erase elt at position pos
-  // throw an exception if pos is not legal
-  // remember to update sz
+void insert(const T& elt, int pos) {
+	if(sz==capacity){ // if size = the capacity it should grow the array
+  		grow();
+  	}
+  	if(pos > sz || pos<0 ){ // the pos greater than the size or less than 0 throw an exception
+  		throw exception();
+  	}
+	for (int i = sz; i > pos; i--) { // looping from the end of the array that the number which in one array is insert it to the the array before it  
+      arr[i] = arr[i - 1];
+	}
+	arr[pos]= elt; // add elt to arr at position pos
+  	sz++;	// increment the size
+}
+  	
+  
   void erase(int pos) {
+  	if(pos>=sz || pos<0) // the pos greater than the size or less than 0 throw an exception
+  	 throw exception();
+  	 
+  	 for(int i=pos ; i<sz-1 ; i++){ // looping on array till the end of the array-1 that the number which in one array will be in the array after it  
+  	 arr[i]=arr[i+1];
   }
-
-	// return first element in vector
+   sz--;   // erase elt at position pos by decrementing the size.
+}
+	
 	T& front() {
+		return arr[0]; // return first element in vector
 	}
 
-	// return last element in vector
 	T& back() {
+		return arr[sz-1];	// return last element in vector
 	}
 
-  // return element at position pos
-  // throw an exception if pos is not legal
   T& at(int pos) {
+  	if(pos>=sz || pos<0) // the pos greater than the size or less than 0 throw an exception
+  throw exception();
+  	
+  	return arr[pos];  // return element at position pos
   }
 
-  // return element at position pos (will break if pos is not legal)
   T& operator[](int pos) {
+  	if(pos>sz || pos<0) // the pos greater than the size or less than 0 throw an exception
+  	throw exception();
+	  	
+  	return arr[pos];// return element at position pos 
+  	
   }
 
-  int size() { return sz; }
+  int size() {
+   return sz; // return the size
+   }
 
-  // return true if vector is empty
   bool empty() {
-  }
+  	if (sz== 0)
+ 	return true; // return true if vector is empty
+ 	else
+ 	return false;// otherwize return false
+}
 };
 
 #endif
+ 
